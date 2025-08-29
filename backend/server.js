a88d39063e5ab8236c816febdf0e22803b3af6ff
Trisha Chikoti <153339361+TrishaChikoti@users.noreply.github.com>
2025-08-29 21:52:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const connectDB = require('./config/db'); // if using MongoDB
+
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
 const rideRoutes = require('./routes/rides');
